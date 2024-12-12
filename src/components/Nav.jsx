0aacd196f8dc50b007b92311c5436dd312def33c
Billa Akshith <billaakshith@gmail.com>
2024-12-12 15:57:57 +0530

    first commisdft

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,43 +1,62 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; 
-function Nav() {
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const myFunction = () => {
+  const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <div>
-      {/* Nav */}
+      {/* Navigation Bar */}
       <div className="w3-bar w3-black">
-      <Link to="/contact" className="w3-bar-item w3-button w3-hide-small">Movie Search</Link> 
-        <Link to="/generatecat" className="w3-bar-item w3-button">Random line Generator</Link>  
-        <Link to="/agepredictor" className="w3-bar-item w3-button w3-hide-small">Age Predictor</Link>  
-        <Link to="/notesapp" className="w3-bar-item w3-button w3-hide-small">Notes App</Link>  
-        
-        
-        {/* Mobile menu button */}
+        <Link to="/contact" className="w3-bar-item w3-button">
+          Movie Search
+        </Link>
+        <Link to="/generatecat" className="w3-bar-item w3-button">
+          Random Line Generator
+        </Link>
+        <Link to="/agepredictor" className="w3-bar-item w3-button w3-hide-small">
+          Age Predictor
+        </Link>
+        <Link to="/notesapp" className="w3-bar-item w3-button w3-hide-small">
+          Notes App
+        </Link>
+
+        {/* Mobile Menu Button */}
         <button
           className="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium"
-          onClick={myFunction}
+          onClick={toggleMenu}
           aria-label="Toggle Menu"
         >
           &#9776;
         </button>
       </div>
 
-      {/* Collapsed mobile menu */}
+      {/* Collapsed Mobile Menu */}
       <div
         id="demo"
-        className={`w3-bar-block w3-black ${isMenuOpen ? "" : "w3-hide"} w3-hide-large w3-hide-medium`}
+        className={`w3-bar-block w3-black ${
+          isMenuOpen ? "" : "w3-hide"
+        } w3-hide-large w3-hide-medium`}
       >
-        <Link to="/about" className="w3-bar-item w3-button">About</Link>  
-        <Link to="/projects" className="w3-bar-item w3-button">Projects</Link> 
-        <Link to="/contact" className="w3-bar-item w3-button">Contact</Link>  
+        <Link to="/contact" className="w3-bar-item w3-button">
+          Movie Search
+        </Link>
+        <Link to="/generatecat" className="w3-bar-item w3-button">
+          Random Line Generator
+        </Link>
+        <Link to="/agepredictor" className="w3-bar-item w3-button">
+          Age Predictor
+        </Link>
+        <Link to="/notesapp" className="w3-bar-item w3-button">
+          Notes App
+        </Link>
       </div>
     </div>
   );
-}
+};
 
-export default Nav;
+export default Navbar;
